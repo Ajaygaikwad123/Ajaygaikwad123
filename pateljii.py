b313a469b092a6946bc54980d58d5bcff327420c
Ajaygaikwad123 <97718740+Ajaygaikwad123@users.noreply.github.com>
2024-10-13 01:02:21 +0530

    Add files via upload

---
+++ pateljii.py
@@ -0,0 +1,8 @@
+# Bot token and admin ID
+TOKEN = "7692263395:AAFz8jM9uNfyqDHx2rT8YOjEIecqunKUlvc"  # Bot Token
+ADMIN_ID = 1613501111  # Admin ID
+
+# Messages
+APPROVED_MESSAGE = "❤️Aap approved hain. Swagat hai 😊 DM--> @patelji_47 !"
+PENDING_APPROVAL_MESSAGE = "Aapka approval pending hai. Kripya admin ke approve hone ka intezaar karein DM--> @patelji_47 \nUser ID ko provide karein. Example: /approve <user_id>."
+ATTACK_BUTTON_MESSAGE = "By  Please enter the target, port, and time in the format:<target> <port> 🚀🚀\n\nAttack shuru karne ke liye button dabaayein. DM--> @patelji_47 "
